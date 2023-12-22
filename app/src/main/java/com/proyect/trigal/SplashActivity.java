cb9c3b5c107bd0125115b67721a8e988aae5e2c2
JeanClix <jp10.02.2003@gmail.com>
2023-12-22 15:33:41 -0500

    SplashActivity

---
+++ app/src/main/java/com/proyect/trigal/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.proyect.trigal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
+
