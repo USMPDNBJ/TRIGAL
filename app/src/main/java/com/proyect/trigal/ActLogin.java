40e37b44b0bf05fbe2364caea9bc905416f6d8e4
JeanClix <jp10.02.2003@gmail.com>
2023-12-28 20:17:24 -0500

    sexo

--- app/src/main/java/com/proyect/trigal/ActLogin.java
+++ app/src/main/java/com/proyect/trigal/ActLogin.java
@@ -3,6 +3,7 @@ package com.proyect.trigal;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,7 +14,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActLogin extends AppCompatActivity {
-    EditText edtCorreo, edtContraseña;
+    private EditText edtCorreo, edtContraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,46 +36,49 @@ public class ActLogin extends AppCompatActivity {
         ImageView imgFacebook = findViewById(R.id.imgFb);
         ImageView imgInstagram = findViewById(R.id.imgIg);
 
-        imgFacebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                abrirUrl("https://www.facebook.com/search/top?q=trigal%20coworking");
-            }
-        });
+        imgFacebook.setOnClickListener(v -> abrirUrl("https://www.facebook.com/search/top?q=trigal%20coworking"));
 
-        imgInstagram.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                abrirUrl("https://www.instagram.com/trigalcoworking/");
-            }
-        });
+        imgInstagram.setOnClickListener(v -> abrirUrl("https://www.instagram.com/trigalcoworking/"));
 
-
-
-        txtLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                abrirvistaAdmin();
-            }
-        });
+        txtLogin.setOnClickListener(v -> abrirVistaAdmin());
     }
 
-    private void abrirvistaAdmin() {
+    private void abrirVistaAdmin() {
         Intent intent = new Intent(ActLogin.this, ActAdmin.class);
         startActivity(intent);
     }
 
     public void IniciarSesion(View view) {
-        String correo, contra;
-        correo = edtCorreo.getText().toString();
-        contra = edtContraseña.getText().toString();
+        String correo = edtCorreo.getText().toString().trim();
+        String contra = edtContraseña.getText().toString().trim();
 
-        if (correo.equals("1") && contra.equals("1")) {
+        // Limpiar errores previos
+        edtCorreo.setError(null);
+        edtContraseña.setError(null);
+
+        // Verificar si algún campo está vacío
+        if (TextUtils.isEmpty(correo)) {
+            edtCorreo.setError("Por favor, ingresa tu correo");
+            return;
+        }
+
+        if (TextUtils.isEmpty(contra)) {
+            edtContraseña.setError("Por favor, ingresa tu contraseña");
+            return;
+        }
+
+        if (validarCredenciales(correo, contra)) {
+            // Verificar si es un usuario y contraseña válidos (solo para ejemplo)
             Intent intent = new Intent(ActLogin.this, MainActivity.class);
             startActivity(intent);
         } else {
-            Toast.makeText(ActLogin.this, "Usuario no válido", Toast.LENGTH_SHORT).show();
+            Toast.makeText(ActLogin.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private boolean validarCredenciales(String correo, String contra) {
+        // Lógica para validar las credenciales (ejemplo)
+        return correo.equals("1") && contra.equals("1");
     }
 
     private void abrirUrl(String url) {
